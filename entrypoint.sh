acd7099d8f07f025680293dce1a8d4444097d177
Neel Phadnis <nphadnis@aerospike.com>
2020-10-19 15:19:28 +0530

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,4 +50,5 @@ while [ $NETLINK_UP -eq 0 ] && [ $NETLINK_COUNT -lt 20 ]; do
 done
 echo "link $NETLINK state $(cat /sys/class/net/${NETLINK}/operstate) in ${NETLINK_COUNT}"
 
-
+echo "executing $@"
+exec "$@"
